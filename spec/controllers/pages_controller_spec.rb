ae204a406ec2aac7fc644c6233ede218896efd1b
skline <spencer.kline@gmail.com>
2011-02-25 19:38:03 -0500

    Added layout lins (with routes)

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -21,6 +21,7 @@ describe PagesController do
       response.body.should_not =~ /<body>\s*<\/body>/
     end
  end
+ 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
@@ -45,5 +46,18 @@ describe PagesController do
                           :content => "#{@base_title} | About")
       end
   end
+  
+  describe "GET 'help'" do
+     it "should be successful" do
+       get 'help'
+       response.should be_success
+     end
+
+       it "should have the right title" do
+         get 'help'
+         response.should have_selector("title",
+                           :content => "#{@base_title} | Help")
+       end
+   end
   
 end
