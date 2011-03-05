0bce0b734e2dbc35cbc8fa611c24be38d97f01c2
skline <spencer.kline@gmail.com>
2011-03-05 14:08:02 -0500

    added sign up form

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -37,6 +37,7 @@ require 'spec_helper'
    end 
     
     describe "GET 'new'" do
+      
       it "should be successful" do
         get 'new'
         response.should be_success 
@@ -52,27 +53,3 @@ require 'spec_helper'
 
 
 
-#require 'spec_helper'
-
-#describe UsersController do
-  #render_views 
-  
-  #describe "Get 'show'"
-   # it "should be successful" do
-    #  get :show
-     # response.should be_success
-    #end
-  #end
-  
-  
-#  describe "GET 'new'" do
- #   it "should be successful" do
-  #    get :new
-   #   response.should be_success
-    #end
-     #it "should have the right title" do
-      #  get :new
-       # response.should have_selector("title",
-        #                    :content => "#{@base_title} | Sign up")
-    #end
-#end
